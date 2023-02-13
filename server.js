af9584d5cd8587f5597ddb63922d8b39bd48ae8c
David's Devel <djgm1206@gmail.com>
2023-02-13 19:23:49 -0400

    fix: Fixe CORS

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const {Server} = require('socket.io');
 const {createServer} = require('http'); 
 
 const port = parseInt(process.env.PORT, 10) || 8082;
+const isDev = process.env.NODE_ENV !== 'production';
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/lakar';
+
+const corsOrigin = isDev ? 'http://localhost:8081' : 'https://lakar-video.vercel.app';
 
 const mongooseOptions = {
   useNewUrlParser: true,
@@ -19,7 +22,7 @@ const handleConnection = require('./lib/handleConnection');
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:8081'
+    origin: corsOrigin
   }
 });
 
